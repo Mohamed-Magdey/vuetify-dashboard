d11b36a31277fe17327bdd2b1b5cad2670c12499
mohamed <ragabmohame6@gmail.com>
2021-09-23 21:02:36 +0200

    Remove unwanted code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,7 @@
 <template>
-  <hello-world />
+  <div>hello</div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
-export default {
-  name: "Home",
-
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
