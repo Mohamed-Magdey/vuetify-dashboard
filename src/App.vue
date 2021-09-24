626d1c65cb521edc028fd12a37ecf1c9ed551789
mohamed <ragabmohame6@gmail.com>
2021-09-24 15:13:38 +0200

    Refactor using the new components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <v-app>
+    <Navbar />
     <v-main>
       <router-view />
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
-export default {
-  name: "App",
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
-  data: () => ({
-    //
-  }),
+export default {
+  components: { Navbar, Footer },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <h1>Welcome to the App!</h1>
-    <router-link to="/"> Login </router-link>
   </div>
 </template>
 
