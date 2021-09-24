e05fbdfc2e80d55834bb211e5894f03424084db5
mohamed <ragabmohame6@gmail.com>
2021-09-24 15:12:57 +0200

    Create Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-app-bar-title>Vuetify Dashboard</v-app-bar-title>
+    <v-spacer />
+    <v-btn text rounded :to="{ name: 'home' }">Home</v-btn>
+    <v-btn text rounded v-bind:to="{ name: 'login' }">Login</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
