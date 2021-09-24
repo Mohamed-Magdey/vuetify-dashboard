5c6f30dc5c52435a51f92602f07b7d5b58e9449f
mohamed <ragabmohame6@gmail.com>
2021-09-24 15:13:09 +0200

    Create Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-footer color="primary lighten-1" padless>
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+        :to="{ name: link }"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+        {{ footerDate }} — <strong>Vuetify Dashboard</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: ["home", "login"],
+  }),
+  computed: {
+    footerDate() {
+      return new Date().getFullYear();
+    },
+  },
+};
+</script>
+
+<style></style>
