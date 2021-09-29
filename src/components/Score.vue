a2d95f88ed97e9c429f00d7b0933644f93cb99ef
mohamed <ragabmohame6@gmail.com>
2021-09-29 14:56:12 +0200

    Create Score component

---
+++ src/components/Score.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row justify="center" align="center" class="mt-5">
+    <v-col cols="12" md="8" lg="4">
+      <v-card>
+        <v-card-title class="primary white--text">
+          <h1 class="headline mx-auto">{{ user.user }}</h1>
+        </v-card-title>
+        <v-card-text class="text-center mt-5"
+          >Your score is {{ user.score }}</v-card-text
+        >
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["user"]),
+  },
+  methods: {
+    ...mapActions("user", ["removeUser"]),
+  },
+  beforeDestroy() {
+    this.removeUser();
+  },
+};
+</script>
