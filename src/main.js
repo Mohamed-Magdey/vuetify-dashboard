6edbe390e28fd2645751e42615ad782f5978911d
mohamed <ragabmohame6@gmail.com>
2021-09-30 13:48:17 +0200

    Add toast-notification

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "nprogress/nprogress.css";
+import VueToast from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-sugar.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueToast, {
+  position: "top",
+});
 
 new Vue({
   router,
